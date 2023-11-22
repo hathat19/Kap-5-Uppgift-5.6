78de75de52f13f63c7eb4d03808d5a9abec8b8e0
humy06001 <humy06001@E-PW0234KS>
2023-11-22 21:54:44 +0100

    Add project files.

---
+++ Kap 5 Uppgift 5.6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace Uppgift5_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Färger
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Clear();
+
+            //-------------------------------------------
+
+            Console.WriteLine("Skriv in heltal seperarade med asterisker \"*\". Produkten kommer räknas ut.");
+            string userInput = Console.ReadLine();
+
+            string[] nrs = userInput.Split("*");
+            long product = 1;
+
+            foreach (string nr in nrs) 
+            {
+                product *= int.Parse(nr);
+            }
+
+            //Utskrift
+            Console.WriteLine($"Produkten av {userInput} är {product}.");
+        }
+    }
+}
+
+/*Skapa ett program där användaren får skriva in en rad med tal separerade med
+asterisker *. Programmet ska beräkna produkten av alla talen, d.v.s. vad alla talen
+multiplicerade med varandra blir. Programmet ska klara av att man skriver in minst 5
+stycken fyrsiffriga tal.*/
